ec5585539cf4ec9ba90955631a535d65f2c69343
JenisseN <63353457+JenisseN@users.noreply.github.com>
2020-06-04 18:27:17 +0200

    Commit Actualización del driver de Chrome

--- src/test/java/runner/browserManager/FirefoxDriverManager.java
+++ src/test/java/runner/browserManager/FirefoxDriverManager.java
@@ -3,8 +3,6 @@ package runner.browserManager;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirefoxDriverManager extends DriverManager{
-
-
     @Override
     protected void createDriver() {
         System.setProperty("webdriver.gecko.driver", "./src/test/resources/geckodriver/geckodriver.exe");
